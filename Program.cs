bbd45530f79ea103f3bdd88addb6b10ac7dadc36
Анатолий <Chepanov199287@gmail.com>
2023-04-05 22:44:15 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_5_2_14
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole(int num = 5)
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {           
+        }
+    }
+}
